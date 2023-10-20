68e4bbd3ff67d8f82f75a2d1f9f83206835980ec
Pedro Evangelista <109436467+TheWayXD@users.noreply.github.com>
2023-10-20 09:38:47 -0300

    Editando os arquivos

--- introducao-javascript/js/principal.js
+++
@@ -1,6 +0,0 @@
-var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista";
-
-var paciente = document.querySelector("#primeiro-paciente")
-
-console.log(paciente);
